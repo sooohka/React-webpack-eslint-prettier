73bb170a73f18ef90e85d38215723e0e957525b2
강수호(sooohka) <shkorea1004@gmail.com>
2022-02-23 11:25:13 +0900

    delete unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,4 @@
-import "./style.css";
-import Counter from "@Components/Counter";
-import img from "./img.jpg";
-import Random from "./assets/random.svg";
-
 function App() {
-  return (
-    <div>
-      <img src={img} alt='testimg' />
-      <Random />
-      <h1>{process.env.NAME}123</h1>
-      <Counter />
-    </div>
-  );
+  return <div>Hello World!</div>;
 }
 export default App;
